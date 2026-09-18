0071e63925b5acd2c1aeb65dbe891468ca23bee9
abhishekkesari9955 <abhishekkeshri9955gmail.com>
2026-09-18 16:09:26 +0530

    add jobs detail and templates

---
+++ jobs/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Job(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    company = models.CharField(max_length=200)
+    location = models.CharField(max_length=100)
+    salary = models.CharField(max_length=100)
+    experience = models.CharField(max_length=100)
+    job_type = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
