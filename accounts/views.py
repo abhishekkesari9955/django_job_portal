84f3ea684a19198ce81ab04428f0ecb93f1172a6
abhishekkesari9955 <abhishekkeshri9955gmail.com>
2026-09-21 10:48:20 +0530

    add register and login page

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,39 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login 
+
+from .forms import RegistrationForm
 
 
 def home(request):
     return render(request, "home.html")
-# Create your views here.
+
+
+def register(request):
+
+    if request.method == "POST":
+
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+
+    else:
+        form = RegistrationForm()
+
+    return render(request, "accounts/register.html", {
+        "form": form
+    })
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            return render(request, "accounts/login.html", {"error": "Invalid username or password."})
+    return render(request, "accounts/login.html")
